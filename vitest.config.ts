eb5326d4cb1d5605797535bfc46b9631b0a77171
Alexis Borges <alexis.borges@estudiantes.utec.edu.uy>
2026-02-27 13:56:58 -0300

    fix(vitest.config):generar archivo coverage-summary.txt

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,7 +4,7 @@ export default defineConfig({
   test: {
     globals: true,
     coverage: {
-      reporter: ['lcov' ,'text', 'json', 'html', 'text-summary'],
+      reporter: ['lcov' ,'text', 'json', 'html', ['text-summary', { file: 'coverage-summary.txt' }]],
       lines: 70,
       functions: 70,
       branches: 70,
